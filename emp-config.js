d522486b98360640447c1441b8114e0beb39ac1f
flyyuan <740004544@qq.com>
2020-12-17 20:31:14 +0800

    feat: 抽离配置

--- emp-config.js
+++ emp-config.js
@@ -1,7 +1,7 @@
+const {resolveApp} = require('@efox/emp-cli/helpers/paths')
+const empConfig = require(`${resolveApp('')}/emp.json`);
 module.exports = ({config, env, empEnv}) => {
   const port = 8080
-  const projectName = 'empReactBase'
-
   const url = {
     prod: 'https://emp-react-base.yourdomain.com/',
     test: 'https://emp-react-base-test.yourdomain.com/',
@@ -16,26 +16,11 @@ module.exports = ({config, env, empEnv}) => {
     args[0] = {
       ...args[0],
       ...{
-        // 项目名称
-        name: projectName,
+        ...empConfig,
         // 暴露项目的全局变量名
-        library: {type: 'var', name: projectName},
+        library: {type: 'var', name: empConfig.projectName},
         // 被远程引入的文件名
         filename: 'emp.js',
-        remotes: {
-          // 远程项目别名:远程引入的项目名
-        },
-        // 需要暴露的东西
-        exposes: {
-          // 别名:组件的路径
-          './App': 'src/App',
-        },
-        // 需要共享的依赖
-        shared: {
-          react: {eager: true, singleton: true, requiredVersion: '^16.13.1'},
-          'react-dom': {eager: true, singleton: true, requiredVersion: '^16.13.1'},
-          'react-router-dom': {requiredVersion: '^5.1.2'},
-        },
       },
     }
     return args
